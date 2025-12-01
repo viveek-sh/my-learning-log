7e76ee8a2e861a4786457669a31d4aaeeb4a6204
viveek-sh <get.viveksahu@gmail.com>
2025-12-01 19:24:58 +0530

    modify test script

---
+++ Daily-Practice/Testing/integration-test/README.md
@@ -0,0 +1,12 @@
+# Getting Started
+
+> Clone the Repo and cd to this module
+
+- ### Install Dependencies
+  ```bash
+  npm install
+  ```
+- ### Run Integration Testing Script
+  ```bash
+  ./scripts/run-integration-test.sh
+  ```
--- Daily-Practice/Testing/integration-test/scripts/run-integration-test.sh
+++ Daily-Practice/Testing/integration-test/scripts/run-integration-test.sh
@@ -4,9 +4,10 @@ set -e
 docker compose up -d
 
 echo "Waiting for DB to be online..."
-./scripts/wait-for-it.sh localhost:5432 -- echo "DB is live now"
+./scripts/wait-for-it.sh postgres://@localhost:5432/ -- echo "DB is live now"
 
 npx prisma migrate dev --name init
+npx prisma generate
 npm run test
 
 docker compose down
