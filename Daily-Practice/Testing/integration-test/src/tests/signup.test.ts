cecd4d09bf193812792b7a034eefabfac64f48cc
viveek-sh <get.viveksahu@gmail.com>
2025-12-02 12:45:19 +0530

    add CI/CD test workflow

--- Daily-Practice/Testing/integration-test/src/tests/signup.test.ts
+++ Daily-Practice/Testing/integration-test/src/tests/signup.test.ts
@@ -1,8 +1,13 @@
-import { describe, it, expect } from "vitest";
+import { beforeAll, describe, it, expect } from "vitest";
 import { app } from "../index.js";
 import request from "supertest";
+import { resetDB } from "./helpers/reset-db.js";
 
 describe("POST /Signup", () => {
+  beforeAll(async () => {
+    console.log("Clearing the DB");
+    await resetDB();
+  });
   it("Should add user info to the DB", async () => {
     const res = await request(app).post("/signup").send({
       name: "Admin",
